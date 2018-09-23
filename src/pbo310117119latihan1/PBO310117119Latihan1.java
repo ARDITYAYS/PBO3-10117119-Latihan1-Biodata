c589aaed24fe50607bfe90e1047946fc88946f9a
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 19:59:31 +0700

    first commit

---
+++ src/pbo310117119latihan1/PBO310117119Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan1;
+
+/**
+ *
+ * @author user
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : PBO-3
+ * Nim       : 10117119
+ * Deskripsi : Program ini hanya menampilkan biodata ke layar
+ */
+public class PBO310117119Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA"+"\n"+"-------");
+        System.out.println("NAMA\t\t: ARDITYA YOGAPUTRA S");
+        System.out.println("ALAMAT\t\t: Jl. Manglayang 4 No.17");
+        System.out.println("TEMPATLAHIR\t: Cirebon");
+        System.out.println("Tanggal Lahir\t: 22 April 1999");
+        System.out.println("--------------------------------------------");
+        System.out.println("HOBI\t\t: Taekwondo");
+        System.out.println("MAKES\t\t: Kimchi");
+        System.out.println("MIKES\t\t: Green Tea");
+    }
+    
+}
